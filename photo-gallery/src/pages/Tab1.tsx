d8de40adcc4cc58f3dcf662632041b523ac48966
Rodrigo López Dato <rodrigo@metabase.com>
2025-10-16 12:46:06 +0200

    add metabase dashboard

--- photo-gallery/src/pages/Tab1.tsx
+++ photo-gallery/src/pages/Tab1.tsx
@@ -8,9 +8,27 @@ import {
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
 
+import {
+  InteractiveDashboard,
+  MetabaseProvider,
+  defineMetabaseAuthConfig,
+} from "@metabase/embedding-sdk-react";
+
+/**
+ * This creates an auth config to pass to the `MetabaseProvider` component.
+ * You'll need to replace the `metabaseInstanceUrl` and the `apiKey` values.
+ */
+const authConfig = defineMetabaseAuthConfig({
+  metabaseInstanceUrl: "http://localhost:3000",
+  apiKey: "YOUR_API_KEY",
+});
+
 const Tab1: React.FC = () => {
   return (
     <IonPage>
+      <MetabaseProvider authConfig={authConfig}>
+        <InteractiveDashboard dashboardId={1} />
+      </MetabaseProvider>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1</IonTitle>
--- photo-gallery/vite.config.ts
+++ photo-gallery/vite.config.ts
@@ -1,18 +1,23 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+  build: {
+    target: "esnext", // you can also use 'es2020' here
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext", // you can also use 'es2020' here
+    },
+  },
+});
